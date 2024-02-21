666c06778071b4f4cb4cc91874b71cf79d9ed827
asadbek-ip <dasturchi6678@gmail.com>
2024-02-21 15:05:12 +0500

    Initial Commit

---
+++ AnimationThemeChange/AnimationThemeChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationThemeChangeApp.swift
+//  AnimationThemeChange
+//
+//  Created by Mac Mini 1 on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationThemeChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationThemeChange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationThemeChange
+//
+//  Created by Mac Mini 1 on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
